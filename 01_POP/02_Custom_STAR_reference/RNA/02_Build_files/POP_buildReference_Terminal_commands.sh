dfa26f8a0d12092f5c5280f7cf9912e627ecc959
DLindenhofer <dominik.lindenhofer@embl.de>
2024-06-09 13:51:08 +0200

    POP_code_upload

---
+++ 01_POP/02_Custom_STAR_reference/RNA/02_Build_files/POP_buildReference_Terminal_commands.sh
@@ -0,0 +1,8 @@
+
+
+
+#Use this to make FASTQ and GTF files for building STAR ref and annotations
+
+python POP_buildReference.py -c POP_cDNA_TAP_REF_seqs.csv -o POP_cDNA_TAP_REF
+										
+
---
+++ 01_POP/02_Custom_STAR_reference/gDNA/02_Build_files/POP_buildReference_Terminal_commands.sh
@@ -0,0 +1,7 @@
+
+
+
+#Use this to make FASTQ and GTF files for building STAR ref and annotations
+python POP_buildReference.py -c POP_gDNA_REF_seqs.csv -o POP_gDNA_REF
+										
+
